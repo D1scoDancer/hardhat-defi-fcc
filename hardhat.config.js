6b9853e15beeb8af60803b7331d7d163958b484a
D1scoDancer <apshulikov@edu.hse.ru>
2023-01-14 16:36:15 +0300

    lending pool added

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,7 +14,7 @@ const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || "key"
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: { compilers: [{ version: "0.8.17" }, { version: "0.4.19" }] },
+    solidity: { compilers: [{ version: "0.8.17" }, { version: "0.6.12" }, { version: "0.4.19" }] },
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,7 +1,25 @@
+const { getNamedAccounts, ethers } = require("hardhat")
 const { getWETH } = require("./getWETH")
 
 async function main() {
     await getWETH()
+    const { deployer } = await getNamedAccounts()
+
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`Lending Pool address: ${lendingPool.address}`)
+}
+
+async function getLendingPool(deployer) {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        deployer
+    )
+
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, deployer)
+
+    return lendingPool
 }
 
 main()
